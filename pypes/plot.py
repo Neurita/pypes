6ef081a378501d57f46216fad647f16a24b5cd08
Alexandre M. S <alexsavio@gmail.com>
2015-11-29 00:03:44 +0100

    enh: move plot function

---
+++ pypes/plot.py
@@ -0,0 +1,15 @@
+"""
+Helper functions to plot nipype workflows
+"""
+
+
+def plot_workflow(wf):
+    """ Plot `wf` nodes in 3 different ways in its working directory.
+
+    Parameters
+    ----------
+    wf: nipype Workflow
+    """
+    # print the graph of the workflow in the working directory
+    wf.write_graph("{}_colored_workflow".format(wf._name), graph2use="colored")
+    wf.write_graph("{}_exec_workflow".format(wf._name),    graph2use="exec")
