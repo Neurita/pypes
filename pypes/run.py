91536efa80f0094311a4f154c1fd89b45b920e4a
Alexandre M. S <alexsavio@gmail.com>
2015-11-29 00:04:15 +0100

    enh: detach run function from wf build

--- pypes/run.py
+++ pypes/run.py
@@ -1,15 +1,11 @@
 """
-
+Helper functions to build base workflow and run them
 """
-
-import os.path as op
 
 import nipype.pipeline.engine   as pe
 from   nipype.interfaces.io     import DataSink
 
 from   .input_files import subject_session_input
-from   .anat        import attach_t1_preprocessing
-from   .pet         import attach_pet_preprocessing
 from   .utils       import extend_trait_list, joinpaths
 
 
@@ -85,75 +81,29 @@ def in_out_workflow(work_dir, data_dir, output_dir, session_names, file_names, s
     return main_wf
 
 
-def run(wf_name="spm_t1_preproc", base_dir="", cache_dir="", output_dir="",
-        year="", plugin="MultiProc", n_cpus=4):
-    """
+def run_workflow(wf, plugin='MultiProc', n_cpus=2, **plugin_kwargs):
+    """ Execute `wf` with `plugin`.
 
     Parameters
     ----------
-    wf_name: str
-
-    base_dir: str
-
-    cache_dir: str
-
-    output_dir: str
-
-    year: str or int
+    wf: nipype Workflow
 
     plugin: str
+        The pipeline execution plugin.
+        See wf.run docstring for choices.
 
     n_cpus: int
+        Number of CPUs to use with the 'MultiProc' plugin.
+
+    plugin_kwargs: keyword argumens
+        Keyword arguments for the plugin if using something different
+        then 'MultiProc'.
     """
-    if not year:
-        data_dir = base_dir
-    else:
-        data_dir = op.join(base_dir, year)
-
-    if not data_dir or not op.exists(data_dir):
-        raise IOError("Expected an existing folder for `data_dir`, got {}.".format(data_dir))
-
-    wfs = {"spm_t1_preproc": attach_t1_preprocessing,
-           "spm_pet_preproc": attach_pet_preprocessing,
-          }
-
-    if wf_name not in wfs:
-        raise ValueError("Expected `wf_name` to be in {}, got {}.".format(list(wfs.keys()),
-                                                                          wf_name))
-
-    # check some args
-    if not output_dir:
-        output_dir = op.join(op.dirname(data_dir), "out", year)
-
-    if not cache_dir:
-        cache_dir = op.join(op.dirname(data_dir), "wd", year)
-
-    # generate the workflow
-    main_wf = in_out_workflow(work_dir=cache_dir,
-                              data_dir=data_dir,
-                              output_dir=output_dir,
-                              session_names=['session_0'],
-                              file_names=['anat_hc.nii.gz', 'pet_fdg.nii.gz'],
-                              subject_ids=None,
-                              input_wf_name='input_files')
-
-    wf = wfs[wf_name](main_wf=main_wf,
-                      data_dir=data_dir,
-                      work_dir=cache_dir,
-                      output_dir=output_dir,)
-
-    # move the crash files folder elsewhere
-    wf.config["execution"]["crashdump_dir"] = op.join(wf.base_dir, wf.name, "log")
-
-    # print the graph of the workflow in the working directory
-    wf.write_graph("{}_colored_workflow".format(wf_name),  graph2use="colored")
-    wf.write_graph("{}_exec_workflow".format(wf_name),     graph2use="exec")
-
     # run the workflow according to `plugin`
     if plugin == "MultiProc" and n_cpus > 1:
         wf.run("MultiProc", plugin_args={"n_procs": n_cpus})
     elif not plugin or plugin is None or n_cpus <= 1:
         wf.run()
     else:
-        wf.run(plugin=plugin)
+        wf.run(plugin=plugin, **plugin_kwargs)
 
