0484cf838830bbafb826c0446c9c11aaf310ea33
Alexandre Manhaes Savio <alexsavio@gmail.com>
2015-12-07 12:59:17 +0100

    enh: add config manager

---
+++ pypes/config.py
@@ -0,0 +1,103 @@
+"""
+Configuration manager for workflow definitions.
+This works over Kaptan:https://github.com/emre/kaptan
+"""
+import os
+import os.path as op
+
+from   kaptan import Kaptan, HANDLER_EXT
+
+from .utils import remove_ext, get_extension
+
+
+class Borg:
+    __shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+    # and whatever else you want in your class -- that's all!
+
+
+class Config(Borg, Kaptan):
+    """
+
+    """
+    def __init__(self):
+        super(Config, self).__init__()
+
+    @staticmethod
+    def _handler_for_ext(ext):
+        """
+
+        Parameters
+        ----------
+        ext: str
+            The file extension without the dot.
+
+        Returns
+        -------
+        handler: a Kaptan.BaseHandler
+        """
+        if ext not in HANDLER_EXT:
+            raise ValueError("Expected a file with an extension "
+                             "in {}, got {}.".format(HANDLER_EXT.keys(), ext))
+
+        return HANDLER_EXT[ext]
+
+    def from_file(self, file_path):
+        """ Returns a Config instance with the data from
+        `file_path`.
+
+        Parameters
+        ----------
+        file_path: str
+            Path to a configuration file.
+            Its extension can be any from config.HANDLER_EXT, i.e.,
+            {'conf': 'ini',
+             'ini': 'ini',
+             'json': 'json',
+             'py': 'file',
+             'yaml': 'yaml',
+             'yml': 'yaml'}
+
+        Returns
+        -------
+        cfg: Config
+        """
+        fpath   = op.abspath(op.expanduser(file_path))
+
+        if not op.isfile(fpath):
+            raise IOError("Could not find file {}.".format(fpath))
+
+        fname   = op.basename(file_path)
+        fext    = get_extension(fname)[1:]
+        fpyname = remove_ext(fname)
+        handler = self._handler_for_ext(fext)
+
+        cpt = Kaptan(handler=handler)
+
+        cwd = op.abspath(op.curdir)
+        is_neighbour = op.samefile(op.dirname(fpath), cwd)
+        if is_neighbour:
+            _ = cpt.import_config(fpyname)
+
+        else:
+            os.chdir(op.dirname(fpath))
+            _ = cpt.import_config(fpyname)
+            os.chdir(cwd)
+
+        self.__dict__.update(cpt.__dict__)
+
+        return self
+
+    def __getitem__(self, item):
+        return self.configuration_data[item]
+
+    def __setitem__(self, key, value):
+        return self.setitem(key, value)
+
+    def setitem(self, key, value):
+        self.configuration_data[key] = value
+
+    def getitem(self, item):
+        return self.configuration_data[item]
