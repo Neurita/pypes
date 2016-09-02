9dbe99207b78fa47f2db20c711834180a43843fb
Alexandre Manhaes Savio <alexsavio@gmail.com>
2016-09-02 16:01:53 +0200

    wip: add dcm2niix node

--- pypes/convert/dcm2nii.py
+++ pypes/convert/dcm2nii.py
@@ -4,17 +4,96 @@ DICOM to Nifti converter node based on dcm2nii.
 """
 import os.path as op
 
+import nipype.pipeline.engine as pe
 from nipype.interfaces.base    import traits
-from nipype.interfaces.dcm2nii import Dcm2nii
+from nipype.interfaces.utility import IdentityInterface
+from nipype.interfaces.dcm2nii import Dcm2niix, Dcm2nii
 
-from .._utils import format_pair_list
-from ..utils  import (spm_tpm_priors_path,
-                      extend_trait_list,
-                      find_wf_node,
-                      get_datasink,
+from ..config import setup_node
+from ..utils  import (get_datasink,
                       get_input_node,
                       remove_ext,
                       get_input_file_name)
+
+
+def dcm2niix_wf(wf_name='dcm2niix'):
+    """Run dcm2niix over one folder with DICOM files.
+
+    Nipype Inputs
+    -------------
+    dcm2niix.in_dcmdir: traits.Dir
+        path to the DICOM images folder.
+
+    Nipype Outputs
+    --------------
+    dcm2niix.bids: (a list of items which are an existing file name)
+
+    dcm2niix.bvals: (a list of items which are an existing file name)
+
+    dcm2niix.bvecs: (a list of items which are an existing file name)
+
+    dcm2niix.converted_files: (a list of items which are an existing file name)
+
+    Returns
+    -------
+    wf: nipype Workflow
+    """
+    # Create the workflow object
+    wf = pe.Workflow(name=wf_name)
+
+    # specify input and output fields
+    in_fields  = ["in_dcmdir",]
+
+    out_fields = ["bids",
+                  "bvals",
+                  "bvecs",
+                  "converted_files"]
+
+    # input node
+    dcm2niix_input = setup_node(IdentityInterface(fields=in_fields,
+                                                  mandatory_inputs=True),
+                                name="dcm2niix_input")
+    # T1 preprocessing nodes
+    dcm2niix = setup_node(Dcm2niix(), name="dcm2niix")
+
+    # output node
+    dcm2niix_output = setup_node(IdentityInterface(fields=out_fields),
+                                 name="dcm2niix_output")
+
+    # Connect the nodes
+    wf.connect([
+                # input
+                (dcm2niix_input, dcm2niix, [("in_dcmdir",  "source_dir"),
+                                           ]),
+
+                # output
+                (dcm2niix, dcm2niix_output, [("bids",            "bids"),
+                                             ("bvals",           "bvals"),
+                                             ("bvecs",           "bvecs"),
+                                             ("converted_files", "converted_files"),
+                                            ]),
+              ])
+
+    return wf
+
+
+def attach_dcm2niix(main_wf, wf_name="dcm2niix"):
+
+    in_files = get_input_node(main_wf)
+    datasink = get_datasink  (main_wf)
+
+    # The workflow box
+    d2n_wf = dcm2niix_wf(wf_name=wf_name)
+
+    main_wf.connect([(in_files, d2n_wf,   [("dcm_dir",                         "dcm2niix_input.in_dcmdir")]),
+                     (d2n_wf,   datasink, [("dcm2niix_output.bids",            "@bids"),
+                                           ("dcm2niix_output.bvals",           "@bvals"),
+                                           ("dcm2niix_output.bvecs",           "@bvecs"),
+                                           ("dcm2niix_output.converted_files", "@converted_files"),
+                                          ],),
+                    ])
+
+    return main_wf
 
 
 def dcm2nii_converter(source_names=traits.Undefined):
@@ -35,6 +114,7 @@ def dcm2nii_converter(source_names=traits.Undefined):
     Note
     ----
     For more info: http://www.mit.edu/~satra/nipype-nightly/interfaces/generated/nipype.interfaces.dcm2nii.html
+    # TODO: an attach function for this node.
     """
     dcm2nii = Dcm2nii()
 
@@ -45,67 +125,3 @@ def dcm2nii_converter(source_names=traits.Undefined):
 
     return dcm2nii
 
-
-def attach_dcm2nii(main_wf, wf_name="dcm2nii_convert"):
-    """ Attach the SPM12 anatomical MRI pre-processing workflow to the `main_wf`.
-
-    Parameters
-    ----------
-    main_wf: nipype Workflow
-
-    wf_name: str
-        Name of the preprocessing workflow
-
-    Nipype Inputs for `main_wf`
-    ---------------------------
-    Note: The `main_wf` workflow is expected to have an `input_files` and a `datasink` nodes.
-
-    input_files.select.anat: input node
-
-    datasink: nipype Node
-
-    Returns
-    -------
-    main_wf: nipype Workflow
-    """
-    in_files = get_input_node(main_wf)
-    datasink = get_datasink  (main_wf)
-
-    # The base name of the 'dicom' file sources for the substitutions
-    anat_fbasename = remove_ext(op.basename(get_input_file_name(in_files, 'anat')))
-
-    #
-    # # dataSink output substitutions
-    # regexp_subst = [
-    #                  (r"/{anat}_.*corrected_seg8.mat$", "/{anat}_to_mni_affine.mat"),
-    #                  (r"/m{anat}.*_corrected.nii$",     "/{anat}_biascorrected.nii"),
-    #                  (r"/wm{anat}.*_corrected.nii$",    "/{anat}_mni.nii"),
-    #                  (r"/y_{anat}.*nii$",               "/{anat}_to_mni_field.nii"),
-    #                  (r"/iy_{anat}.*nii$",              "/{anat}_to_mni_inv_field.nii"),
-    #                  (r"/mwc1{anat}.*nii$",             "/{anat}_gm_mod_mni.nii"),
-    #                  (r"/mwc2{anat}.*nii$",             "/{anat}_wm_mod_mni.nii"),
-    #                  (r"/mwc3{anat}.*nii$",             "/{anat}_csf_mod_mni.nii"),
-    #                  (r"/mwc4{anat}.*nii$",             "/{anat}_nobrain_mod_mni.nii"),
-    #                  (r"/c1{anat}.*nii$",               "/{anat}_gm.nii"),
-    #                  (r"/c2{anat}.*nii$",               "/{anat}_wm.nii"),
-    #                  (r"/c3{anat}.*nii$",               "/{anat}_csf.nii"),
-    #                  (r"/c4{anat}.*nii$",               "/{anat}_nobrain.nii"),
-    #                  (r"/c5{anat}.*nii$",               "/{anat}_nobrain_mask.nii"),
-    #                ]
-    # regexp_subst = format_pair_list(regexp_subst, anat=anat_fbasename)
-    # datasink.inputs.regexp_substitutions = extend_trait_list(datasink.inputs.regexp_substitutions,
-    #                                                          regexp_subst)
-    #
-    # # input and output anat workflow to main workflow connections
-    # main_wf.connect([(in_files, t1_wf,    [("anat",                                  "bias_correction.input_image")]),
-    #                  (t1_wf,    datasink, [("warp_anat.normalized_files",            "anat.@mni")],),
-    #                  (t1_wf,    datasink, [("new_segment.modulated_class_images",    "anat.tissues.@warped"),
-    #                                        ("new_segment.native_class_images",       "anat.tissues.@native"),
-    #                                        ("new_segment.transformation_mat",        "anat.transform.@linear"),
-    #                                        ("new_segment.forward_deformation_field", "anat.transform.@forward"),
-    #                                        ("new_segment.inverse_deformation_field", "anat.transform.@inverse"),
-    #                                        ("new_segment.bias_corrected_images",     "anat.@biascor"),
-    #                                       ]),
-    #                 ])
-
-    return main_wf
