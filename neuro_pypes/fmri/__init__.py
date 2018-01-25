951d15eecf09638145c4b6a3c6c32b7180b5cd41
Alexandre Savio <alexsavio@gmail.com>
2018-01-25 17:33:30 +0100

    style(fmri/rest): rename fmri.rest to fmri.resting

--- neuro_pypes/fmri/__init__.py
+++ neuro_pypes/fmri/__init__.py
@@ -2,5 +2,5 @@
 
 from .warp import attach_spm_warp_fmri_wf
 from .clean import attach_fmri_cleanup_wf
-from .rest import attach_rest_preprocessing, attach_rest_grptemplate_preprocessing
+from .resting import attach_rest_preprocessing, attach_rest_grptemplate_preprocessing
 from .grouptemplate import attach_spm_fmri_grouptemplate_wf
